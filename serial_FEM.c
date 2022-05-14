68be0a1d45231cc18db3867bdfc84ab09a2b18ed
ThomasIngel <thomas.ingel@uni-ulm.de>
2022-05-14 16:28:31 +0200

    initial commit

---
+++ serial_FEM.c
@@ -0,0 +1,12 @@
+/*
+This is a placeholder file for our main function
+*/
+int main() {
+    /*
+    A possible workflow is e.g.:
+        1. Construct mesh
+        2. Assemble stiffness matrix and RHS
+        3. Solve system of linear equations
+        4. check solution
+    */
+}
