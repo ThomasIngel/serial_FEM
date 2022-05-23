c16083c5ddbc9bb819b1616ab88a705a3c2a10e6
Oli Kabis <oli@kabis-online.de>
2022-05-23 14:10:13 +0200

    allreduce

--- parallel/parallel.c
+++ parallel/parallel.c
@@ -23,6 +23,18 @@ double u_D( double x[2])
   return ( x[0] * x[1] );
 }
 
+void* make_global(index *c,index nlocal,double *rhs_loc,double *rhs_glob){
+  for(int i=0;i<nlocal;i++){
+    rhs_glob[c[i]] = rhs_loc[i];
+  }
+}
+
+void* make_local(index *c,index nlocal,double *rhs_glob,double *rhs_loc){
+  for(int i=0;i<nlocal;i++){
+    rhs_loc[i] = rhs_glob[c[i]] ;
+  }
+}
+
 int main(int argc, char *argv[]) {
 
   int numprocs;
@@ -57,19 +69,55 @@ int main(int argc, char *argv[]) {
 
     meshsplit(H, metra, anz_dom);
   }
-  mesh_trans* test =  scatter_meshes(metra,MPI_COMM_WORLD,anz_dom,ncoord);
+  mesh_trans* mesh_loc =  scatter_meshes(metra,MPI_COMM_WORLD,anz_dom,ncoord);
 
   sed* S;
   S = malloc (sizeof(sed));
-  S = sed_sm_build(test);
+  S = sed_sm_build(mesh_loc);
   /*printf("\nProcessor %d lokale SM:\n", myid);
   sed_print(S,0);*/
-  double* b = calloc(test->ncoord_loc, sizeof(double));
-  mesh_trans_rhs(test,b,F_vol, g_Neu);
+  double* b = calloc(mesh_loc->ncoord_loc, sizeof(double));
+  mesh_trans_rhs(mesh_loc,b,F_vol, g_Neu);
 
   printf("\nProcessor %d rhs:\n", myid);
-  for(int i=0;i<test->ncoord_loc;i++){
+  for(int i=0;i<mesh_loc->ncoord_loc;i++){
     printf("%lg\n",b[i]);
+  }
+
+  double* rhs_global = calloc(mesh_loc->ncoord_glo, sizeof(double));
+  make_global(mesh_loc->c,mesh_loc->ncoord_loc,b,rhs_global);
+
+  if(myid==0){
+    /*printf("\nProcessor %d rhs:\n", myid);
+    for(int i=0;i<mesh_loc->ncoord_loc;i++){
+      printf("%lg\n",b[i]);
+    }*/
+    printf("\nProcessor %d made global rhs:\n", myid);
+    for(int i=0;i<mesh_loc->ncoord_glo;i++){
+      printf("%lg\n",rhs_global[i]);
+    }
+  }
+
+  double recv_global[mesh_loc->ncoord_glo];
+  MPI_Allreduce(
+    rhs_global,
+    recv_global,
+    9,
+    MPI_DOUBLE,
+    MPI_SUM,
+    MPI_COMM_WORLD);
+  double* rhs_local = calloc(mesh_loc->ncoord_loc, sizeof(double));
+  make_local(mesh_loc->c,mesh_loc->ncoord_loc,rhs_global,rhs_local);
+
+  if(myid==0){
+    printf("\nProcessor %d rhs after allreduce:\n", myid);
+    for(int i=0;i<mesh_loc->ncoord_glo;i++){
+      printf("%lg\n",recv_global[i]);
+    }
+    /*printf("\nProcessor %d rhs after local:\n", myid);
+    for(int i=0;i<mesh_loc->ncoord_loc;i++){
+      printf("%lg\n",rhs_local[i]);
+    }*/
   }
 
   MPI_Finalize();
