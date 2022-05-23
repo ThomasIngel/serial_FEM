c0c86921a95a092d447d7940004ce850c15f101f
ThomasIngel <thomas.ingel@uni-ulm.de>
2022-05-23 17:05:34 +0200

    newest version

--- code_material/Source/cg_seriell.c
+++ code_material/Source/cg_seriell.c
@@ -1,58 +1,66 @@
+// cg_seriell Version2
+
 #include <math.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "blas_level1.c"
-#include "sed_spmv.c"
+#include "hpc.h"
+#include "blas_level1.h"
 
 void
-cg_seriell(size_t n,
-	   const sed *A, const double *b, double *u, double tol) {
-	// A   - stiffness matrix (sed Format!)
-	// b   - righthand side
-	// u   - inital guess for solution
-	// tol - Toleranz (stopping criteria)
-	
-	double r[n];
-	blasl1_icopy(b,r,n,1.);		//kopiert b in r (also r=b)
-	
-	// r = b - A*u    r = r-A*b
-	sed_spmv(A,u,r,-1.,1.);		//Ergebnis steht in r
+cg_seriel(size_t n,
+           const sed *A, const double *b, double *u, double tol) {
+        // A   - stiffness matrix (sed Format!)
+        // b   - righthand side
+        // u   - inital guess for solution
+        // tol - Toleranz (stopping criteria)
 
-	// sigma = r'*r (Skalarprodukt)
-	double sigma_0 = blasl1_ddot(r,r,n);
-	double sigma = sigma_0;
+        double r[n];
+        blasl1_dcopy(b,r,(index) n,1.);         //kopiert b in r (also r=b)
 
-	// d = r
-	double d[n];
-	blasl1_icopy(r,d,n,1.);	
+        // r = b - A*u    r = r-A*b
+        sed_spmv_adapt(A,u,r,-1.0);             //Ergebnis steht in r
 
-	do {
-		// ad = A*d (damit nur 1x Matrixprodukt)
-		double ad[n];	
-		sed_spmv(A,d,ad,1.,0.);
+        // sigma = r'*r (Skalarprodukt)
+        double sigma_0 = blasl1_ddot(r,r,n);
+        double sigma = sigma_0;
 
-		// alpha = sigma/(d*ad)
-		double dad = blasl1_ddot(d,ad,n);
-		double alpha = sigma/dad;
-			
-		// Update: u = u + alpha*d
-		blasl1_daxpy(u,d,(index) n,alpha,1.);
+        // d = r
+        double d[n];
+        blasl1_dcopy(r,d,(index) n,1.0);
 
-		// r = r - alpha*ad
-		blasl1_daxpy(r,ad,(index) n,-alpha,1.);		
+        size_t k = 0;
+        do {
+                k++;
+                // ad = A*d (damit nur 1x Matrixprodukt)
+                double *ad = calloc(n, sizeof(double));         //statt double ad[n]    ad mit 0en initiieren calloc
+                if (ad == NULL) {
+                        printf("Error! memory not allocated.");
+                        exit(0);
+                }
+                sed_spmv_adapt(A,d,ad,1.);
 
-		// sigma_neu = r' * r
-		double sigma_neu = blasl1_ddot(r,r,n);
+                // alpha = sigma/(d*ad)
+                double dad = blasl1_ddot(d,ad,n);
+                double alpha = sigma / dad;
 
-		// d = (sigma_neu/sigma)*d + r
-		blasl1_daxpy(d,r,(index) n,1.,sigma_neu/sigma);
-		
-		// sigma = sigma_neu
-		sigma = sigma_neu;
+                // Update: u = u + alpha*d
+                blasl1_daxpy(u, d, (index) n, alpha, 1.0);
 
-	} while (sqrt(sigma/sigma_0) > tol);
-		
+                // r = r - alpha*ad
+                blasl1_daxpy(r, ad, (index) n, -alpha, 1.0);
+
+                // sigma_neu = r' * r
+                double sigma_neu = blasl1_ddot(r,r,n);
+
+                // d = (sigma_neu/sigma)*d + r
+                blasl1_daxpy(d, r, (index) n, 1.0, sigma_neu / sigma);
+
+                // sigma = sigma_neu
+                sigma = sigma_neu;
+                free(ad);
+              //printf("k = %d \t norm = %10g\n", k, sqrt(sigma));
+        } while (sqrt(sigma) > tol);
 }
