3e49ff3303f0343af319d3e8fde196a6e5821f05
Oli Kabis <oliver.kabis@uni-ulm.de>
2022-06-03 12:55:31 +0200

    refined mesh debug

--- code_material/Source/get_refined_mesh.c
+++ code_material/Source/get_refined_mesh.c
@@ -1,22 +1,25 @@
 #include "hpc.h"
 
-// Load Mesh from file and refine norefine times
+// Load Hesh from file and refine norefine times
 mesh *get_refined_mesh(int norefine){
 
-    // Load and initialize Mesh
-    mesh **M;
-    M = malloc ( (norefine+1) * sizeof(mesh));
-    char *fname = "../code_material/Problem/problem1";
-    M[0] = mesh_load(fname);   
-    mesh_getEdge2no(M[0]->nelem, M[0]->elem, &M[0]->nedges, &M[0]->edge2no);
-    M[0]->fixed = mesh_getFixed(M[0]->ncoord, M[0]->bdry, M[0]->nbdry, &M[0]->nfixed);
+    // Load and initialize Hesh
+    mesh* I;
+    mesh* H;
+
+    I = malloc (sizeof(mesh));
+    char *fname = "../code_material/Iroblem/problem1";
+    I = mesh_load(fname);   
+    mesh_getEdge2no(I->nelem, I->elem, &I->nedges, &I->edge2no);
+    I->fixed = mesh_getFixed(I->ncoord, I->bdry, I->nbdry, &I->nfixed);
     
-    // Refine Mesh
+    // Refine Hesh
     for (int k = 0; k<norefine; k++){
-        M[k+1] = mesh_refine(M[k]);
-        mesh_getEdge2no(M[k+1]->nelem, M[k+1]->elem, &M[k+1]->nedges, &M[k+1]->edge2no);
-        M[k+1]->fixed = mesh_getFixed(M[k+1]->ncoord, M[k+1]->bdry, M[k+1]->nbdry, &M[k+1]->nfixed);
-        free(M[k]);
+        H = mesh_refine(I);
+        mesh_getEdge2no(H->nelem, H->elem, &H->nedges, &H->edge2no);
+        H->fixed = mesh_getFixed(H->ncoord, H->bdry, H->nbdry, &H->nfixed);
+        I = mesh_free(I);
+        I = H;
     }
-    return(M[norefine]);
+    return(H);
 }
